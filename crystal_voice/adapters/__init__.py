62eaccbce64df25f80ad243215ffb27dfaeea828
thonfreelancer24-creator <thon.freelancer24@gmail.com>
2026-08-23 13:52:36 +0800

    Register native WeSep recovery adapter

--- crystal_voice/adapters/__init__.py
+++ crystal_voice/adapters/__init__.py
@@ -2,10 +2,12 @@ from crystal_voice.adapters.diagnostic import SameTakeDiagnosticAdapter
 from crystal_voice.adapters.clearervoice import ClearerVoiceSpExPlusAdapter
 from crystal_voice.adapters.external import WeSepAdapter
 from crystal_voice.adapters.restoration import SpExPlusMossFormerSRAdapter
+from crystal_voice.adapters.wesep_native import NativeWeSepAdapter
 
 ADAPTERS = {
     "diagnostic": SameTakeDiagnosticAdapter,
     "spexplus": ClearerVoiceSpExPlusAdapter,
     "spexplus-sr": SpExPlusMossFormerSRAdapter,
     "wesep": WeSepAdapter,
+    "wesep-native": NativeWeSepAdapter,
 }
