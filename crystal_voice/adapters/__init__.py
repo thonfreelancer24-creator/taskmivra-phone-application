4d3c224f051ad0f1a5bc82cb3862431207b28175
thonfreelancer24-creator <thon.freelancer24@gmail.com>
2026-08-23 18:24:31 +0800

    Register WeSep speech-enhancement recovery adapter

--- crystal_voice/adapters/__init__.py
+++ crystal_voice/adapters/__init__.py
@@ -3,6 +3,7 @@ from crystal_voice.adapters.clearervoice import ClearerVoiceSpExPlusAdapter
 from crystal_voice.adapters.external import WeSepAdapter
 from crystal_voice.adapters.restoration import SpExPlusMossFormerSRAdapter
 from crystal_voice.adapters.wesep_native import NativeWeSepAdapter
+from crystal_voice.adapters.wesep_enhanced import WeSepMossFormerSEAdapter
 
 ADAPTERS = {
     "diagnostic": SameTakeDiagnosticAdapter,
@@ -10,4 +11,5 @@ ADAPTERS = {
     "spexplus-sr": SpExPlusMossFormerSRAdapter,
     "wesep": WeSepAdapter,
     "wesep-native": NativeWeSepAdapter,
+    "wesep-se": WeSepMossFormerSEAdapter,
 }
